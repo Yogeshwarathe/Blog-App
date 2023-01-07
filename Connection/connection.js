aa4448e4b128342b9b109a80340b617a0c23c7b4
Yogeshwarathe <yogesh18@gmail.com>
2023-01-07 17:00:17 +0530

    here i make function and franten page use by html

--- Connection/connection.js
+++ Connection/connection.js
@@ -1,20 +1,18 @@
+
 // this function use to token janret
 function Token_janreter_fun(Gmail,jwt){
     const Token = jwt.sign({Gmail},"SECRETKEY")
     // console.log(NewToken);
     return Token
 }
-// var NewToken = Token_janreter_fun({Gmail:"yogesh@gmail.com"});
-// console.log(fun1);
 
                                            
 
 // This function use to get data
-function TodayDate(){
-    const date_ob = new Date();
-    const date = ("0" + date_ob.getDate()).slice(-2);
-    const month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-    const year = date_ob.getFullYear();
+function TodayDate(new_date){
+    const date = ("0" + new_date.getDate()).slice(-2);
+    const month = ("0" + (new_date.getMonth() + 1)).slice(-2);
+    const year = new_date.getFullYear();
     return year + "-" + month + "-" + date
 }
 
@@ -22,11 +20,10 @@ function TodayDate(){
 // console.log(date);
 
 // This function use to get time
-function Time(){
-    const date_ob = new Date();
-    const hours = date_ob.getHours();
-    const minutes = date_ob.getMinutes();
-    const seconds = date_ob.getSeconds();
+function Time(new_date){
+    const hours = new_date.getHours();
+    const minutes = new_date.getMinutes();
+    const seconds = new_date.getSeconds();
     return hours + ":" + minutes + ":" + seconds
 }
 
@@ -34,5 +31,37 @@ function Time(){
 // console.log(time);
 
 
+function sign_up_page_function(res){
+    res.sendFile(__dirname + "/" + "/Htmlfile/SignUpPage.html");
+}
 
-module.exports = {Token_janreter_fun,TodayDate,Time};
+function login_page_function(res){
+    res.sendFile(__dirname + "/" + "/Htmlfile/LoginPage.html");
+}
+
+function create_post_function(res){
+    res.sendFile(__dirname + "/" + "/Htmlfile/CreatePost.html");
+}
+
+function like_dislike_page_function(res){
+    res.sendFile(__dirname + "/" + "/Htmlfile/LikeDislikePage.html");
+}
+
+function total_user_information_function(res){
+    res.sendFile(__dirname + "/" + "/Htmlfile/TotalUserInformation.html");
+}
+
+
+
+
+
+
+module.exports ={
+                    Token_janreter_fun,
+                    TodayDate,Time,
+                    sign_up_page_function,
+                    login_page_function,
+                    create_post_function,
+                    like_dislike_page_function,
+                    total_user_information_function
+                };
